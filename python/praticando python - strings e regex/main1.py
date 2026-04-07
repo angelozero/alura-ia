846a2007e58e887885b1330e983c209171791b9d
Angelo Zero <angelo.akm@gmail.com>
2026-04-07 16:07:25 -0300

    python

---
+++ python/praticando python - strings e regex/main1.py
@@ -0,0 +1,10 @@
+def main():
+    nome = "Angelo Zero"
+    print(nome[1::4])
+    print(nome[:2])
+    print(nome[2:])
+    print(nome[::4])
+    print(nome[::2])
+
+if __name__ == "__main__":
+    main()
