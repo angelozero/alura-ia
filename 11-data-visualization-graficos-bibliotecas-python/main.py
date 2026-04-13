feb945a3111050fc27fd972fb344a6f3ce350296
Angelo Zero <angelo.akm@gmail.com>
2026-04-13 15:14:34 -0300

    machine learning

--- 11-data-visualization-graficos-bibliotecas-python/main.py
+++ 11-data-visualization-graficos-bibliotecas-python/main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from 11-data-visualization-graficos-bibliotecas-python!")
+# %%
+# Importando a lib do pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# Recuperando dados do csv
+df = pd.read_csv("./data/imigrantes_canada.csv")
+
+# %%
+# Exibindo dados da planilha
+df.head()
+
+# %%
+# Analisando as tendencias de imigração
+df.set_index("País", inplace=True)
+anos = list(map(str, range(1980, 2014)))
+brasil = df.loc['Brasil', anos]
+brasil_dict = {'ano': brasil.index.tolist(), 'imigrantes': brasil.values.tolist()}
+dados_brasil = pd.DataFrame(brasil_dict)
 
 
-if __name__ == "__main__":
-    main()
+# %%
+# Criando um grafico com PyPlot
+plt.figure(figsize=(7.5, 3))
+plt.title('Imigração Brasil <> Canadá')
+plt.xlabel('Ano')
+plt.ylabel('Imigrantes')
+plt.plot(dados_brasil['ano'], dados_brasil['imigrantes'])
+plt.xticks(dados_brasil['ano'][::5])
+plt.show()
+
+
+
+# %%
+fig, ax = plt.subplots(figsize=(7.5, 3))
+ax.plot(dados_brasil['ano'], dados_brasil['imigrantes'])
+ax.set_title('Imigração Brasil <-> Canadá')
+ax.xaxis.set_major_locator(plt.MultipleLocator(5))
+plt.show()
+# %%
--- 12-pandas-conhecendo-biblioteca/main.py
+++ 12-pandas-conhecendo-biblioteca/main.py
@@ -1,6 +1,89 @@
-def main():
-    print("Hello from 12-pandas-conhecendo-biblioteca!")
+# %%
+import pandas as pd
+
+# %%
+# Importando o csv
+url = 'https://raw.githubusercontent.com/alura-cursos/pandas-conhecendo-a-biblioteca/main/base-de-dados/aluguel.csv'
+dados = pd.read_csv(url, sep=';')
+
+# %%
+# Exibindo a tabela do csv
+dados.head()
+
+# %%
+# Exibindo os tipos de dados
+type(dados)
+
+# %%
+# Media de valor da coluna 'Valor'
+dados['Valor'].mean()
+
+# %%
+# Agrupando 'Valor' pela coluna 'Tipo' e formando o valor
+dados_formatados = dados.groupby('Tipo')[['Valor']].mean(numeric_only=True).sort_values('Valor').style.format({'Valor': 'R$ {:,.2f}'})
+
+print(dados_formatados)
+
+df_preco_tipo = dados.groupby('Tipo')[['Valor']].mean(numeric_only=True).sort_values('Valor')
+
+# %%
+# Exibindo grafico
+df_preco_tipo.plot(kind='barh', figsize=(14, 10), color='purple')
+
+# %%
+# Recuperando imoveis
+imoveis = dados['Tipo'].unique()
+
+# %%
+imoveis_comerciais = ['Conjunto Comercial/Sala', 
+                      'Prédio Inteiro', 'Loja/Salão',
+                      'Galpão/Depósito/Armazém', 
+                      'Casa Comercial', 
+                      'Terreno Padrão', 
+                      'Box/Garagem',
+                      'Loja Shopping/ Ct Comercial', 
+                      'Chácara', 
+                      'Loteamento/Condomínio',
+                      'Sítio', 
+                      'Pousada/Chalé', 
+                      'Hotel', 
+                      'Indústria']
+
+# %%
+# Recuperando apenas imoveis residencias
+imoveis_residenciais = dados_filtrados = dados.query('@imoveis_comerciais not in Tipo')
+imoveis_residenciais
+
+imoveis_residenciais['Tipo'].unique()
+
+# %%
+# Exibindo grafico com apenas imoveis residenciais
+imoveis_residenciais.groupby('Tipo')[['Valor']].mean(numeric_only=True).sort_values('Valor').plot(kind='barh', figsize=(14, 10), color='purple')
 
 
-if __name__ == "__main__":
-    main()
+# %%
+# Calulando o percentual de cada tipo de imovel na base 
+imoveis_residenciais_percentual= imoveis_residenciais['Tipo'].value_counts(normalize=True).to_frame()
+
+# %%
+imoveis_residenciais_percentual.plot(kind='bar', figsize=(14, 10), color='green', xlabel='Tipo de Imóvel', ylabel='Percentual', title='Percentual de cada tipo de imóvel residencial')
+
+# %%
+# Trabalhando apenas com Apartamentos
+apartamentos = imoveis_residenciais.query('Tipo == "Apartamento"')
+
+# %%
+# Lidando com dados nulos
+apartamentos.isnull().sum()
+
+# %%
+apartamentos_nao_nulos = apartamentos.fillna(0)
+apartamentos_nao_nulos.isnull().sum()
+
+
+# %%
+# Removendo linhas com valor == 0
+registros_a_remover = apartamentos_nao_nulos.query('Valor == 0 | Condominio == 0').index
+apartamentos.drop(apartamentos_nao_nulos, axis=0, inplace=True)
+
+# %%
