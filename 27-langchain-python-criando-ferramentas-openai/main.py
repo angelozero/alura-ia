16f3560bc264a6a476b3cd677d7a703c47b3e5d0
Angelo Zero <angelo.akm@gmail.com>
2026-04-29 14:10:21 -0300

    .

--- 27-langchain-python-criando-ferramentas-openai/main.py
+++ 27-langchain-python-criando-ferramentas-openai/main.py
@@ -1,10 +1,18 @@
-# from factory_service import get_chat_model
+from factory_service import get_chat_model
+from langchain_core.prompts import PromptTemplate
 
 def main():
-    # chat_model = get_chat_model()
-    # response = chat_model("What is the capital of France?")
-    # print(response)
-    print("Done!")
+    
+    
+    prompt_model = PromptTemplate(
+        template="What is the capital of {country}?",
+        input_variables=["country"]
+    )
+    
+    
+    llm = get_chat_model()
+    response = llm.invoke(prompt_model.format(country="France"))
+    print(response.content)
 
 
 if __name__ == "__main__":
